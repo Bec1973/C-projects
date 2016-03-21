8ce5399dbc23d802787069ebcdebcc1c33de92e0
Bec1973 <rmalagui@outlook.com>
2016-03-22 05:47:43 +0800

    Added files via upload

---
+++ ProgAct28/ProgAct28/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgAct28
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Global variable to end loop
+        bool endLoop = true;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            listOutput.Items.Clear();
+
+            while(endLoop)
+            {
+                counter++;
+                listOutput.Items.Insert(0, "Loop number " + counter);
+                Application.DoEvents();
+                // requires System.Threading libraries
+                System.Threading.Thread.Sleep(500);
+
+            }
+            endLoop = true;
+        } // end of btnStart_Click
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            endLoop = false;
+            listOutput.Items.Insert(0, "Loop stopped");
+        } // end of btnStop_Click
+    }
+}
